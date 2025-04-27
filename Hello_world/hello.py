eb44323bb48f484d9956761e1775f90c108d9b61
sumitkumar2299 <sumitofficialme@gmail.com>
2025-04-27 13:21:47 +0530

    starting a new journey of python with cisco

---
+++ Hello_world/hello.py
@@ -0,0 +1,9 @@
+print("hello world");
+
+# where do function come from 
+
+# from python itself (inbuilt function)
+# from module  -> some module comes with python and 
+# other may require seperate installation
+
+# from your code => you can make your own function
