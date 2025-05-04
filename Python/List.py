462e236883e2a79b4d8bb0099c1faab496a9fcc1
sumitkumar2299 <sumitofficialme@gmail.com>
2025-05-04 23:32:02 +0530

    memory management of list and tuples

---
+++ Python/List.py
@@ -0,0 +1,36 @@
+# lists in python 
+
+# indexing in python 
+# index starts from zero 
+
+List= ["sumit",7.0,1992]
+print(List[0]);
+
+# list can any dataType and also nested list 
+
+list1 = ["sumit","rahul","xyz",["abc","def"],("A",1)];
+print(list1)
+
+
+# operation in list 
+
+list2 = ["sumit","abc","def","ghi",5]
+
+# list slicing 
+# 5 will always be excluded 
+x = list2[3:5];
+print(x);
+
+
+
+list = ["sumit","kumar",44,7.0,]
+print(list);
+
+# use append to add element in the list 
+
+list.append('sumit1');
+print(list);
+
+# if i have to add more than one element in the list?
+y = list.append(["john","doe"]);
+print(y);
