9633245a6f2d78cfff49b41990b3e67d895c1f0f
sumitkumar2299 <sumitofficialme@gmail.com>
2025-05-09 22:05:08 +0530

    learning file handling

---
+++ file.py
@@ -0,0 +1,25 @@
+with open("example.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+with open("example.txt", "w") as file:
+    file.write("Hello, World!")
+
+
+with open("example.txt", "a") as file:
+    file.write("\nAppended line.")
+
+with open("example.txt", "a") as file:
+    file.write("\nAppended line.")
+
+
+with open("example.txt", "a") as file:
+    file.write("\nAppended line.")
+
+
+with open("example.txt", "a") as file:
+    file.write("\nAppended line.")
+
+
+with open("example.txt", "a") as file:
+    file.write("\nAppended line.")
