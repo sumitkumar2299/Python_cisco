de52af3f758d1d30b43fe0cd0b0e9ffee127e07b
sumitkumar2299 <sumitofficialme@gmail.com>
2025-05-14 20:08:03 +0530

    solving dictionary assignment

---
+++ Python_training/Dictionary_assignment/q1.py
@@ -0,0 +1,9 @@
+
+
+keys = ['Ten','Twenty','Thirty']
+values = [10,20,30];
+
+# converting list into dictionary 
+
+dictionary = dict(zip(keys,values))
+print(dictionary)
---
+++ Python_training/Dictionary_assignment/q3.py
@@ -0,0 +1,5 @@
+dict = {}
+
+employees = ['ram','amit']
+defaults:{"designation":"developer","salary":8000}
+
